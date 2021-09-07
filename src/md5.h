ccdfcd338528822c4fabbd69ba4687514162971d
GeneKong <gyx_edu@qq.com>
2021-09-07 10:56:42 +0800

    适配esp32的md5头

--- src/md5.h
+++ src/md5.h
@@ -4,15 +4,18 @@
 #ifdef __cplusplus
  extern "C" {
 #endif
-
 typedef struct  
 {  
     unsigned int count[2];  
     unsigned int state[4];  
     unsigned char buffer[64];     
 }MD5_CTX;  
-   
-                           
+#if defined(ESP_PLATFORM)
+void MD5Init(MD5_CTX *context);  
+void MD5Update(MD5_CTX *context,unsigned char *input,unsigned int inputlen);  
+void MD5Final(unsigned char digest[16], MD5_CTX *context);
+#else
+                     
 #define F_(x,y,z) ((x & y) | (~x & z))
 #define G(x,y,z) ((x & z) | (y & ~z))  
 #define H(x,y,z) (x^y^z)  
@@ -44,13 +47,14 @@ typedef struct
           }
 void MD5Init(MD5_CTX *context);  
 void MD5Update(MD5_CTX *context,unsigned char *input,unsigned int inputlen);  
-void MD5Final(MD5_CTX *context,unsigned char digest[16]);  
+void MD5Final(MD5_CTX *context,unsigned char digest[16]);
+
 void MD5Transform(unsigned int state[4],unsigned char block[64]);  
 void MD5Encode(unsigned char *output,unsigned int *input,unsigned int len);  
 void MD5Decode(unsigned int *output,unsigned char *input,unsigned int len);
 
+#endif   
 #ifdef __cplusplus
  }
 #endif
-   
 #endif
